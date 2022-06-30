91578ec1132a7728103259b502b3048d6faa60ae
Lewis Jang <lewis.aedes@gmail.com>
2022-06-30 17:10:34 +0800

    fixed print bug

--- preprocess.py
+++ preprocess.py
@@ -12,15 +12,28 @@ for spk, files, save_dir_path in zip(config.voice_speaker, config.voice_files, c
     coded_sps_T = mel.transpose_in_list(coded_sps)
     norm_mel, norm_mean, norm_std = mel.coded_sps_normalization_fit_transoform(coded_sps_T)
 
-    print('Log Pitch %s' % spk)
-    print('Mean: %f, Std: %f' % (mean, std))
-    print("org")
-    print(coded_sps_T.shape, coded_sps.shape, mean.shape, std.shape)
-    print("norm")
-    print(norm_mel.shape, norm_mean.shape, norm_std.shape)
-
-    if not os.path.exists(save_dir_path):
-        os.makedirs(save_dir_path)
-
     np.savez(os.path.join(save_dir_path, spk + "_org.npz"), mel=coded_sps_T, mean=mean, std=std)
     np.savez(os.path.join(save_dir_path, spk + '_normalization.npz'), mel=norm_mel, mean=norm_mean, std=norm_std)
+
+
+    print('Log Pitch %s' % spk)
+    print('Mean: %f, Std: %f' % (mean, std))
+
+    print("norm")
+    for i in [norm_mel, norm_mean, norm_std]:
+        print("start")
+        pi = i
+        while type(pi) is list:
+            print(len(pi))
+            pi = pi[0]
+
+        print("end")
+
+    if not os.path.exists(save_dir_path):
+        print("start")
+        pi = i
+        while type(pi) is list:
+            print(len(pi))
+            pi = pi[0]
+
+        print("end")
