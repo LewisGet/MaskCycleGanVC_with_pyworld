ac3df0cd47ecf8e17f0e2adc4c3d1d823e167418
Lewis Jang <lewis.aedes@gmail.com>
2022-07-07 00:42:54 +0800

    batch feed world encode, make memory runnable
    Signed-off-by: Lewis Jang <lewis.aedes@gmail.com>

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ import os
 sample_rate = 16000
 n_frames = 64
 frame_period = 5.0
-coded_dim = 32
+coded_dim = 128
 device = 'cuda'
 load_epoch = 0
 load_model_path = os.path.join(os.sep, "home", "results", "debug", "ckpts")
 gpu_ids = [0]
 wav_max_size = sample_rate * 60 * 5
+preprocess_max_batch = 100
 
 voice_speaker = ["lewis", "kevin"]
 voice_files = list()
--- preprocess.py
+++ preprocess.py
@@ -2,38 +2,118 @@ import os
 import mel
 import config
 import numpy as np
+import pickle
+import glob
 
 
-for spk, files, save_dir_path in zip(config.voice_speaker, config.voice_files, config.voice_preprocess_dir):
-    wavs = mel.load_wavs(files, config.sample_rate)
-    f0s, timeaxes, sps, aps, coded_sps = mel.world_encode_data(wavs, config.sample_rate, config.frame_period, config.coded_dim)
-    mean, std = mel.logf0_statistics(f0s)
+def pack_up(values):
+    pack_value = list()
+    overflow = list()
 
-    coded_sps_T = mel.transpose_in_list(coded_sps)
-    norm_mel, norm_mean, norm_std = mel.coded_sps_normalization_fit_transoform(coded_sps_T)
+    for value in values:
+        for index in range(0, value.shape[-1], config.n_frames):
+            start, end = index, index + config.n_frames
+            slice_value = value[:, start:end]
 
-    np.savez(os.path.join(save_dir_path, spk + "_org.npz"), mel=coded_sps_T, mean=mean, std=std)
-    np.savez(os.path.join(save_dir_path, spk + '_normalization.npz'), mel=norm_mel, mean=norm_mean, std=norm_std)
+            if slice_value.shape[-1] == config.n_frames:
+                pack_value.append(slice_value)
+            else:
+                overflow.append(slice_value)
+
+    # overflow dataset join to build new dataset
+    overflow = np.concatenate([*overflow], axis=1)
+
+    for index in range(0, overflow.shape[-1], config.n_frames):
+        start, end = index, index + config.n_frames
+        slice_value = overflow[:, start:end]
+
+        if slice_value.shape[-1] == config.n_frames:
+            pack_value.append(slice_value)
+
+    return pack_value
 
 
-    print('Log Pitch %s' % spk)
-    print('Mean: %f, Std: %f' % (mean, std))
+def save_pickle(variable, fileName):
+    with open(fileName, 'wb') as f:
+        pickle.dump(variable, f)
 
-    print("norm")
-    for i in [norm_mel, norm_mean, norm_std]:
-        print("start")
-        pi = i
-        while type(pi) is list:
-            print(len(pi))
-            pi = pi[0]
 
-        print("end")
+def load_pickle_file(fileName):
+    with open(fileName, 'rb') as f:
+        return pickle.load(f)
 
-    if not os.path.exists(save_dir_path):
-        print("start")
-        pi = i
-        while type(pi) is list:
-            print(len(pi))
-            pi = pi[0]
 
-        print("end")
+def batch_world_encode():
+    for spk, files, save_dir_path in zip(config.voice_speaker, config.voice_files, config.voice_preprocess_dir):
+        wavs = mel.load_wavs(files, config.sample_rate)
+
+        for batch in range(0, len(wavs), config.preprocess_max_batch):
+            start, end = batch, batch + config.preprocess_max_batch
+            batch_of_wavs = wavs[start:end]
+
+            f0s, timeaxes, sps, aps, coded_sps = mel.world_encode_data(batch_of_wavs, config.sample_rate, config.frame_period, config.coded_dim)
+
+            file_path = os.path.join(save_dir_path, f"{spk}_{str(batch).zfill(5)}_")
+            save_pickle(f0s, file_path + "f0s.pickle")
+            save_pickle(timeaxes, file_path + "timeaxes.pickle")
+            save_pickle(sps, file_path + "sps.pickle")
+            save_pickle(aps, file_path + "aps.pickle")
+            save_pickle(coded_sps, file_path + "coded_sps.pickle")
+
+
+def get_mean_std():
+    for spk, save_dir_path in zip(config.voice_speaker, config.voice_preprocess_dir):
+        f0s_paths = glob.glob(os.path.join(save_dir_path, "*_f0s.pickle"))
+
+        all_f0s = list()
+
+        for path in f0s_paths:
+            f0s = load_pickle_file(path)
+            all_f0s = [*all_f0s, *f0s]
+
+        mean, std = mel.logf0_statistics(f0s)
+
+        np.save(os.path.join(save_dir_path, f"{spk}_mean.npy"), mean)
+        np.save(os.path.join(save_dir_path, f"{spk}_std.npy"), std)
+
+
+def get_mel():
+    for spk, save_dir_path in zip(config.voice_speaker, config.voice_preprocess_dir):
+        coded_sps_paths = glob.glob(os.path.join(save_dir_path, "*_coded_sps.pickle"))
+
+        all_coded_sps = list()
+
+        for path in coded_sps_paths:
+            coded_sps = load_pickle_file(path)
+            all_coded_sps = [*all_coded_sps, *coded_sps]
+
+        coded_sps_T = mel.transpose_in_list(coded_sps)
+
+        save_pickle(coded_sps_T, os.path.join(save_dir_path, f"{spk}_mel.pickle"))
+
+
+def get_mel_pack_up():
+    for spk, save_dir_path in zip(config.voice_speaker, config.voice_preprocess_dir):
+        mel_batchs = load_pickle_file(os.path.join(save_dir_path, f"{spk}_mel.pickle"))
+
+        mel_pack = pack_up(mel_batchs)
+        np.save(os.path.join(save_dir_path, f"{spk}_mel_format.npy"), mel_pack)
+
+
+def get_norm():
+    for spk, save_dir_path in zip(config.voice_speaker, config.voice_preprocess_dir):
+        mels = np.load(os.path.join(save_dir_path, f"{spk}_mel_format.npy"))
+
+        norm_mel, norm_mean, norm_std = mel.coded_sps_normalization_fit_transoform(mels)
+
+        np.save(os.path.join(save_dir_path, f"{spk}_norm_mel_format.npy"), norm_mel)
+        np.save(os.path.join(save_dir_path, f"{spk}_norm_mean.npy"), norm_mean)
+        np.save(os.path.join(save_dir_path, f"{spk}_norm_std.npy"), norm_std)
+
+
+if __name__ == '__main__':
+    batch_world_encode()
+    get_mean_std()
+    get_mel()
+    get_mel_pack_up()
+    get_norm()
